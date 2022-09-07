a3b6d5e5e228acd18ca686320a1bc517acfcc5e9
leesh1993 <dltjdgnsok1@naver.com>
2022-09-07 20:20:32 +0900

    커스텀 필터 구현

--- src/main/java/me/hoon/apigatewayservice/filter/CustomFilter.java
+++ src/main/java/me/hoon/apigatewayservice/filter/CustomFilter.java
@@ -1,4 +1,4 @@
-package me.hoon.apigatewayservice.config;
+package me.hoon.apigatewayservice.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
